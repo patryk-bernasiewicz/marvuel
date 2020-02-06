df98f707bc109bbca9f7a6ea997bcf075303bfec
Patryk Bernasiewicz <kontakt@patrykb.pl>
2020-02-06 15:41:14 +0100

    changed name of page 'Home' to 'Heroes'

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Heroes from '../views/Heroes.vue';
 
 Vue.use(VueRouter);
 
 export const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Heroes',
+    component: Heroes
   }
   // {
   //   path: "/about",
